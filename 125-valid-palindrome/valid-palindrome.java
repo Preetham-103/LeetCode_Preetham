23c34d849a65c6915cb47479bf08bc4dfc4bc694
Preetham Kumar Dundigalla <140134551+Preetham-103@users.noreply.github.com>
2024-07-16 11:56:36 +0530

    Time: 15 ms (25.78%) | Memory: 45.4 MB (15.85%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+
+        if (s==" ") {
+            return true;
+        }
+
+        for (int i = 0; i < n / 2; i++) {
+            if (arr[i] != arr[n - i - 1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
